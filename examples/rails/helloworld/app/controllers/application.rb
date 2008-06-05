e5a329fef3dd9bd78b49806dc2056e22c81ad29c
David Li <taweili@yahoo.com>
2008-06-05 21:02:30 +0800

    add rails skelton

---
+++ examples/rails/helloworld/app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '615ba770a5f8d80ac0e07030deabbca7'
+end
