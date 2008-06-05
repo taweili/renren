8cac0bf844f3605a921656fddc7f1af68ccedb62
David Li <taweili@yahoo.com>
2008-06-05 21:43:28 +0800

    add rspec

--- examples/rails/helloworld/script/tunnel.sh
+++ examples/rails/helloworld/script/tunnel.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-exec ssh -C -v -o TCPKeepAlive=true admin@fb.onceup.com -Rwww.featha.com:7000:127.0.0.1:3000
+exec ssh -C -v -o TCPKeepAlive=true admin@www.featha.com -Rwww.featha.com:5100:127.0.0.1:3000
