f3ac426b5d75667fa492189703a551bffbd4507d
David Li <david@Macintosh.local>
2008-06-07 18:04:12 +0800

    setup env

--- examples/rails/helloworld/config/environment.rb
+++ examples/rails/helloworld/config/environment.rb
@@ -61,6 +61,9 @@ Rails::Initializer.run do |config|
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
+  
+   # Disable request forgery protection in test environment
+  config.action_controller.allow_forgery_protection = false
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
