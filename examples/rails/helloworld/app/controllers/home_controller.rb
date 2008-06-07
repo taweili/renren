c27cf8c6d8d025181bab24f7dce32a54f852ab29
David Li <david@Macintosh.local>
2008-06-07 19:15:44 +0800

    setup session

--- examples/rails/helloworld/app/controllers/home_controller.rb
+++ examples/rails/helloworld/app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 require "xiaonei/service"
+require "xiaonei/session"
 
 class HomeController < ApplicationController
   def index
-    service = Xiaonei::Service.new(ENV['XIAONEI_API_KEY'])
-    @res = service.post(:method => "xiaonei.users.getInfo", :api_key => ENV['XIAONEI_API_KEY'], :session_key => params[:xn_sig_session_key], :v => "1.0", :uids => params[:xn_sig_user])
+    session = Xiaonei::Session.new()
+    session.session_key = params[:xn_sig_session_key]
+    @res = session.invoke_method("xiaonei.users.getInfo", :uids => params[:xn_sig_user], :fields => [:name, :sex, :hometown_location].join(","))
+    logger.debug("#{@res.inspect} - #{@res.body}")
+    @res = session.invoke_method("xiaonei.friends.getFriends")
     logger.debug("#{@res.inspect} - #{@res.body}")
   end
 end
