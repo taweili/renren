cafb379dc6961d7ba2572ff03eff4060890735f4
David Li <david@Macintosh.local>
2008-06-08 16:20:39 +0800

    dump all information

--- examples/rails/helloworld/app/controllers/home_controller.rb
+++ examples/rails/helloworld/app/controllers/home_controller.rb
@@ -6,8 +6,9 @@ class HomeController < ApplicationController
     session = Xiaonei::Session.new()
     session.session_key = params[:xn_sig_session_key]
     
-    @user = session.invoke_method("xiaonei.users.getInfo", :uids => "223065501", :fields => [:name, :sex, :hometown_location].join(","))
+    @user = session.invoke_method("xiaonei.users.getInfo", :uids => "229615263", :fields => Xiaonei::User.attr_names.join(","))
     @user = @user.first
+    logger.debug("#{@user.inspect}")
     @friends = session.invoke_method("xiaonei.friends.getFriends")
   end
 end
