246e13193debf41e6b14d174b7cde3558cb0d93a
David Li <david@Macintosh.local>
2008-06-12 11:42:35 +0800

    add new API methods

---
+++ lib/xiaonei/models/album.rb
@@ -0,0 +1,25 @@
+module Xiaonei
+  class Album < Model
+    
+    def self.elm_name
+      "album"
+    end
+    
+    def self.attr_names
+      [
+       :aid,
+       :uid,
+       :url,
+       :name,
+       :size,
+       :create_time,
+       :update_time
+      ]
+    end
+
+    for a in attr_names
+      attr_accessor a
+    end
+    
+  end
+end
