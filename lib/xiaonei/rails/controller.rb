91c00eb63c606fc36cc761ec4a282ef718c9acef
David Li <david@Macintosh-5.local>
2008-08-01 00:17:25 -0700

    support xiaonei_session in session to support iframe

--- lib/xiaonei/rails/controller.rb
+++ lib/xiaonei/rails/controller.rb
@@ -9,7 +9,16 @@ module Xiaonei
       attr_reader :xiaonei_session 
       
       def set_xiaonei_session
-        @xiaonei_session ||= Session.new(params)
+        if @xiaonei_session.nil?
+          @xiaonei_session = Session.new(params)
+          if @xiaonei_session.in_iframe
+            session[:xiaonei_session] = @xiaonei_session
+          end
+          if @xiaonei_session.session_key.nil?
+            @xiaonei_session = session[:xiaonei_session]
+          end
+        end
+        @xiaonei_session
       end
       
       module ClassMethods
