05cddd0764286c3753790a084cb916309d8132b9
Richard Huang <flyerhzm@gmail.com>
2010-03-30 16:13:03 +0800

    use one generator renren

--- generators/renren/renren_generator.rb
+++ generators/renren/renren_generator.rb
@@ -2,6 +2,7 @@ class RenrenGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.file 'config/renren.yml', 'config/renren.yml'
+      m.file "public/xd_receiver.html", "public/xd_receiver.html"
     end
   end
 end
